81a5636696c84b00616a4cb56a01f6790fed14b0
ThuanHCMUT <tieuho036@gmail.com>
2022-03-30 11:58:28 +0700

    update

--- mvc/views/home.php
+++ mvc/views/home.php
@@ -11,9 +11,9 @@
                 <div class="col l-2-4 m-4 c-12 room__status__body__list" style="border-radius: 20px;">
                     <div class="room__status__body__list__title">Gas</div>
                         <h1 id="gas-now">
-                            <?php 
-                                echo $gas_now;
-                            ?>
+                            <h5 style="text-align:center; margin: 5px auto; color:red">Nồng độ Gas hiện tại: <?php echo $gas_now;?>
+                            </h5>
+                            
                         </h1>
                         <div class="wrapper" style="display:flex; width: 100%; justify-content: center; align-items: center;" >
                             <div class="container_alert chart" data-size="200" data-value="<?php echo ($gas_now/1023*100); ?>" data-arrow="up">
@@ -38,10 +38,11 @@
                 </div>
                 <div class="col l-2-4 m-4 c-12 room__status__body__list" style="border-radius: 20px; ">
                     <div class="room__status__body__list__title">Light</div>
-
+                    <!-- <img src="https://cdn-icons.flaticon.com/png/512/3430/premium/3430793.png?token=exp=1648614143~hmac=292332732fea5b71c12d96f96eec37ef" alt="light_on" style="height: 100px; width:100px" hidden> -->
+                    <img id="lightonoff" src="https://cdn-icons.flaticon.com/png/512/3351/premium/3351798.png?token=exp=1648614110~hmac=f7d46da26a8cf81c51fee5d0283acade" alt="light_off" style="height: 100px; width:100px; display: block; margin: auto; margin-top: 30%">
                     <form action="<?php echo BASE_URL?>/lightswitch/switch" method="POST" id='my-form'>
-                        <label class="switch">
-                            <input name='switch1' value="" <?php if ($light_now == 1) {echo 'checked';} ?> type="checkbox" id='checked_light'>
+                        <label class="switch" style="display: block; margin: auto; margin-top: 10%">
+                            <input name='switch1' value="" <?php if ($light_now == 1) {echo 'checked';} ?> type="checkbox" id='checked_light' >
                             <span class="slider round"></span>
                         </label>
                         <!-- <button id="submitbtn" type="submit">Kennads</button> -->
@@ -49,7 +50,7 @@
 
                 </div>
                 <div class="col l-12 m-12 c-12">
-                        <a href="<?php echo BASE_URL?>/record/update"><button class="btn5-hover btn5">Update</Button></a>
+                        <a href="<?php echo BASE_URL?>/record/update"><button class="btn5-hover btn5" style="display: block; margin: 10% auto;">Update</Button></a>
                 </div>
             </div>
         </div>
@@ -89,23 +90,32 @@
 
     // });
 
+    
     document.addEventListener('DOMContentLoaded', function() {
         var checkedLight = $('#checked_light') 
         var myForm = $('#my-form')
         var submitBtn = $('#submitbtn')
-        
-        isChecked = checkedLight.prop('checked')
-        if (isChecked) checkedLight.val('1')
-        else checkedLight.val('0')
+        var lightonoff = document.getElementById('lightonoff')
 
+        isChecked = checkedLight.prop('checked')
+        if (isChecked) {
+            checkedLight.val('1')
+            lightonoff.src = "https://cdn-icons.flaticon.com/png/512/3430/premium/3430793.png?token=exp=1648614143~hmac=292332732fea5b71c12d96f96eec37ef"
+        }
+        else {
+            checkedLight.val('0')
+            lightonoff.src = "https://cdn-icons.flaticon.com/png/512/3351/premium/3351798.png?token=exp=1648614110~hmac=f7d46da26a8cf81c51fee5d0283acade"
+        }
         checkedLight.change(function() {
             if (checkedLight.val() == '1') {
                 checkedLight.val('0')
+                lightonoff.src = "https://cdn-icons.flaticon.com/png/512/3351/premium/3351798.png?token=exp=1648614110~hmac=f7d46da26a8cf81c51fee5d0283acade"
                 myForm.submit()
                 return
             }
             else {
                 checkedLight.val('1')
+                lightonoff.src = "https://cdn-icons.flaticon.com/png/512/3430/premium/3430793.png?token=exp=1648614143~hmac=292332732fea5b71c12d96f96eec37ef"
                 myForm.submit()
             }
         })
