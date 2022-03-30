0347640c51df6578ba5f5c6e679e230e7f92bb9f
Kentinyhippoo <93818026+Kentinyhippoo@users.noreply.github.com>
2022-03-30 11:22:57 +0700

    buzzerBtn

--- mvc/views/home.php
+++ mvc/views/home.php
@@ -10,16 +10,26 @@
                 </div>
                 <div class="col l-2-4 m-4 c-12 room__status__body__list" style="border-radius: 20px;">
                     <div class="room__status__body__list__title">Gas</div>
-                        <h1 id="gas-now">
-                            <?php 
-                                echo $gas_now;
-                            ?>
-                        </h1>
-                        <div class="wrapper" style="display:flex; width: 100%; justify-content: center; align-items: center;" >
-                            <div class="container_alert chart" data-size="200" data-value="<?php echo ($gas_now/1023*100); ?>" data-arrow="up">
-                            </div>
+                    <h1 id="gas-now">
+                        <?php 
+                            echo $gas_now;
+                        ?>
+                    </h1>
+                    <div class="wrapper" style="display:flex; width: 100%; justify-content: center; align-items: center;" >
+                        <div class="container_alert chart" data-size="200" data-value="<?php echo ($gas_now/1023*100); ?>" data-arrow="up">
                         </div>
-                        <script src="<?php echo BASE_URL ?>/public/js/gas_alert.js"></script>
+                    </div>
+
+                    <form action="<?php echo BASE_URL?>/buzzerswitch/switch" method="POST" id='my-buzzer-form'>
+                        <label class="switch">
+                            <input name='buzzer_switch1' value="" <?php if ($buzzer_now == 2) {echo 'checked';} ?> type="checkbox" id='checked_buzzer'>
+                            <span class="slider round"></span>
+                        </label>
+                        <!-- <button id="submitbtn" type="submit">Kennads</button> -->
+                    </form>
+
+
+                    <script src="<?php echo BASE_URL ?>/public/js/gas_alert.js"></script>
                     <div class="charkbtn">
                         <a href="#"><button class="btn5-hover btn5">Chart</Button></a>
                     </div>
@@ -39,9 +49,9 @@
                 <div class="col l-2-4 m-4 c-12 room__status__body__list" style="border-radius: 20px; ">
                     <div class="room__status__body__list__title">Light</div>
 
-                    <form action="<?php echo BASE_URL?>/lightswitch/switch" method="POST" id='my-form'>
+                    <form action="<?php echo BASE_URL?>/lightswitch/switch" method="POST" id='my-light-form'>
                         <label class="switch">
-                            <input name='switch1' value="" <?php if ($light_now == 1) {echo 'checked';} ?> type="checkbox" id='checked_light'>
+                            <input name='light_switch1' value="" <?php if ($light_now == 1) {echo 'checked';} ?> type="checkbox" id='checked_light'>
                             <span class="slider round"></span>
                         </label>
                         <!-- <button id="submitbtn" type="submit">Kennads</button> -->
@@ -91,24 +101,45 @@
 
     document.addEventListener('DOMContentLoaded', function() {
         var checkedLight = $('#checked_light') 
-        var myForm = $('#my-form')
-        var submitBtn = $('#submitbtn')
+        var myLightForm = $('#my-light-form')
+
+        var checkBuzzer = $('#checked_buzzer')
+        var myBuzzerForm = $('#my-buzzer-form')
+
         
-        isChecked = checkedLight.prop('checked')
-        if (isChecked) checkedLight.val('1')
+        isLightChecked = checkedLight.prop('checked')
+        if (isLightChecked) checkedLight.val('1')
         else checkedLight.val('0')
+
+        isBuzzerChecked = checkBuzzer.prop('checked')
+        if (isBuzzerChecked) checkBuzzer.val('2')
+        else checkBuzzer.val('3')
 
         checkedLight.change(function() {
             if (checkedLight.val() == '1') {
                 checkedLight.val('0')
-                myForm.submit()
+                myLightForm.submit()
                 return
             }
             else {
                 checkedLight.val('1')
-                myForm.submit()
+                myLightForm.submit()
             }
         })
+
+        checkBuzzer.change(function() {
+            if (checkBuzzer.val() == '2') {
+                checkBuzzer.val('3')
+                myBuzzerForm.submit()
+                return
+            }
+            else {
+                checkBuzzer.val('2')
+                myBuzzerForm.submit()
+            }
+        })
+
+
     })
 
 </script>
