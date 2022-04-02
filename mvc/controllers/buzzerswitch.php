9d240bc13c64cb644b52940071a39ef3c2e75ee9
Kentinyhippoo <93818026+Kentinyhippoo@users.noreply.github.com>
2022-04-02 22:20:32 +0700

    fix buzzer insert data

--- mvc/controllers/buzzerswitch.php
+++ mvc/controllers/buzzerswitch.php
@@ -53,6 +53,24 @@
 
             curl_close($ch);
 
+            $tbl_records = 'records';
+            $homemodel = $this->load->model('homemodel');
+            $arrContextOptions=array(
+                "ssl"=>array(
+                    "verify_peer"=>false,
+                    "verify_peer_name"=>false,
+                ),
+            );  
+            $response = file_get_contents($url, false, stream_context_create($arrContextOptions));
+    
+            $result = json_decode($response); 
+            $buzzer = array(
+                "DATAS" => $result[0]->value,
+                "TIMES" => $result[0]->created_at,
+                "DevID" => 3
+            );
+            $res = $homemodel->insertdata($tbl_records, $buzzer);
+
             header('Location: ' .BASE_URL );
 
         }
