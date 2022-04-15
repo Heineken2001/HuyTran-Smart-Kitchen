557f63e64a22a48e70119bd781942a28e930bd8a
ThuanHCMUT <tieuho036@gmail.com>
2022-04-15 14:14:48 +0700

    update

--- mvc/views/cpanel/usermng.php
+++ mvc/views/cpanel/usermng.php
@@ -5,17 +5,18 @@
                     <div class="mng__title">
                         User Management
                     </div>
+                    <div class="col l-12 m-12 c-12" style="border-radius: 20%;">
                     <div class="tbl-header">
                         <table cellpadding="0" cellspacing="0" border="0">
                           <thead>
                             <tr>
-                              <th>No</th>
-                              <th>Name</th>
-                              <th>Email</th>
-                              <th>Location</th>
-                              <th>Contract Code</th>
-                              <th>Gas Bound</th>
-                              <th>Delete Account</th>
+                              <th style="color: black; font-weight:bold">No</th>
+                              <th style="color: black; font-weight:bold">Name</th>
+                              <th style="color: black; font-weight:bold">Email</th>
+                              <th style="color: black; font-weight:bold">Location</th>
+                              <th style="color: black; font-weight:bold">Contract Code</th>
+                              <th style="color: black; font-weight:bold">Gas Bound</th>
+                              <th style="color: black; font-weight:bold">Delete Account</th>
                             </tr>
                           </thead>
                         </table>
@@ -30,7 +31,7 @@
                               <td><?php echo $value['EMAIL']?></td>
                               <td><?php echo $value['ADDRESS']?></td>
                               <td><?php echo $value['ContID']?></td>
-                              <td style="color: red; text-align: center;">
+                              <td id="gasbound">
                                 <?php echo $value['GASBOUND']?>
                               </td>
                               <td><a href="<?php echo BASE_URL?>/admin/deleteuser/<?php echo $value['ContID']?>"><button  class="btn btn-log">Delete</button></a></td>
@@ -40,7 +41,7 @@
                           </tbody>
                         </table>
                       </div>
-                    </section>
+                    </div>
             </div>
         </div>
 
