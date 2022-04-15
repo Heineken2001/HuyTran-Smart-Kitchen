629d0d7cd79373886a8a2daedfe3626e8d639103
ThuanHCMUT <tieuho036@gmail.com>
2022-04-15 13:41:28 +0700

    update

--- mvc/views/cpanel/admin.php
+++ mvc/views/cpanel/admin.php
@@ -1,13 +1,17 @@
-<div class="row room__status__body">
+<div class="grid wide container">
+    <div style="text-align:center; margin-top: 0">
+        <h1 style="margin-top: 100px">Administrator</h1>
+    </div>
+    <div class="row room__status__body" style="margin-top: 50px">
             <div class="grid">
                 <div class="row">
-                    <div class="col l-6 m-6 c-12 room__status__body__list">
+                    <div class="col l-6 m-6 c-12 room__status__body__list" style="border-radius: 20px;">
                         <div class="room__status__body__list__title">Summary Report</div>
                         <div class="charkbtn">
                             <a href="#"><button class="btn5-hover btn5">Details</Button></a>
                         </div>
                     </div>
-                    <div class="col l-6 m-6 c-12 room__status__body__list">
+                    <div class="col l-6 m-6 c-12 room__status__body__list" style="border-radius: 20px;">
                         <div class="room__status__body__list__title">User Management</div>
                         <div class="charkbtn">
                             <a href="<?php echo BASE_URL ?>/admin/usermanagement"><button class="btn5-hover btn5">Manage</Button></a>
@@ -15,6 +19,5 @@
                     </div>
                 </div>
             </div>
-        </div>
-
     </div>
+</div>
