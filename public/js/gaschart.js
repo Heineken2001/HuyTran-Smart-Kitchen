0299f76a2b46179c6eca86c3d438c2723c4a9cc3
ThuanHCMUT <tieuho036@gmail.com>
2022-04-19 20:48:06 +0700

    Update gaschart.js

--- public/js/gaschart.js
+++ public/js/gaschart.js
@@ -1,5 +1,5 @@
-var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+var yValues = [500,600,700,800,900,1000,510,520,300,400,700,1023];
+var xValues = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 new Chart("myChart", {
   type: "line",
@@ -16,7 +16,7 @@ new Chart("myChart", {
   options: {
     legend: {display: false},
     scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
+      yAxes: [{ticks: {min: 0, max:1100}}],
     }
   }
 });
