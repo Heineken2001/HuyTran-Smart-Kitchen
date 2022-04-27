70baaafc66914946fc42c387f171e2a05fe7d9cb
ThuanHCMUT <tieuho036@gmail.com>
2022-04-28 04:39:07 +0700

    Update summaryreport.php

--- mvc/views/summaryreport.php
+++ mvc/views/summaryreport.php
@@ -226,7 +226,7 @@
                         <br>
                         <h5 style="color: black; margin: 10px auto; margin-bottom: 50px">Số lần nồng độ Gas vượt ngưỡng trong ngày <?php echo date("d-m-Y",strtotime($date_time))?> là: <?php echo $danger_gas?>.</h5>
                     </div>
-                    <div class="" style="float: right; margin-top: 8%; margin-right: 9%">
+                    <div class="" style="float: right; margin-top: 10%; margin-right: 9%">
                         <h1 class="login__header__brand">SmartKitchen by CoderCodon</h1>
                     </div>
                     
